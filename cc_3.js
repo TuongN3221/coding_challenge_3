a18bafa64dfeba5f4d8efab3a264a6a4eee25c8c
Tuong Nguyen <yolotube21@yahoo.com>
2025-01-28 19:11:16 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+// Task 1 - Product Price Management
+
+let prices = [50.99, 23.99, 34.99, 64.99, 85.99, 100.99];// Declares array of intial prices
+prices.push(69.98);// Adds new price to array
+
+prices.shift();// removes the first price from array (50.99)
+console.log(prices);
